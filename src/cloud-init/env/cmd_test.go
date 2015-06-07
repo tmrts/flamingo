bdcae7e88cd597bc928e8e65b0569493ecb62943
Tamer Tas <tamertas@outlook.com>
2015-06-07 05:47:03 +0300

    Add command execution wrapper tests

---
+++ src/cloud-init/env/cmd_test.go
@@ -0,0 +1,25 @@
+package env
+
+import "testing"
+
+func TestExecuteCommand(t *testing.T) {
+	out, err := ExecuteCommand("echo", "test")
+	if err != nil {
+		t.Fatalf("error executing echo command -> %v", err)
+	}
+
+	if out != nil {
+		t.Errorf("wrong output for echo command -> expected: %v, got: %v", "this", err)
+	}
+}
+
+func TestExecutingACommandWithErrors(t *testing.T) {
+	out, err := ExecuteCommand("", "")
+	if err != nil {
+		t.Fatalf("expected: error when executing an invalid command, got: no errors")
+	}
+
+	if out == "" {
+		t.Errorf("expected: error message regarding the faulty executing of an invalid command, got: no error messages")
+	}
+}
