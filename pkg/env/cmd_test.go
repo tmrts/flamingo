7a6da86c2c97ab7027ecbbb59e461cb1dea481b0
Tamer Tas <tamertas@outlook.com>
2015-06-13 08:28:07 +0300

    Refactor command execution tests

--- pkg/env/cmd_test.go
+++ pkg/env/cmd_test.go
@@ -2,24 +2,20 @@ package env
 
 import "testing"
 
+func TestExecutingACommandWithErrors(t *testing.T) {
+	_, err := ExecuteCommand("", "")
+	if err == nil {
+		t.Fatalf("expected: error when executing an invalid command, got: no errors")
+	}
+}
+
 func TestExecuteCommand(t *testing.T) {
-	out, err := ExecuteCommand("echo", "test")
+	out, err := ExecuteCommand("echo", "-n", "fi fye fo fum")
 	if err != nil {
 		t.Fatalf("error executing echo command -> %v", err)
 	}
 
-	if out != "" {
+	if out != "fi fye fo fum" {
 		t.Errorf("wrong output for echo command -> expected: %v, got: %v", "this", err)
-	}
-}
-
-func TestExecutingACommandWithErrors(t *testing.T) {
-	out, err := ExecuteCommand("", "")
-	if err != nil {
-		t.Fatalf("expected: error when executing an invalid command, got: no errors")
-	}
-
-	if out == "" {
-		t.Errorf("expected: error message regarding the faulty executing of an invalid command, got: no error messages")
 	}
 }
