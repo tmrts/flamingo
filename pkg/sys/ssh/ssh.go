e0a8673a6d35f2d35c9f141f0b9d3a3762e69e19
Tamer Tas <tamertas@outlook.com>
2015-06-21 12:29:01 +0300

    Implement ssh configuration

---
+++ pkg/sys/ssh/ssh.go
@@ -0,0 +1,67 @@
+package ssh
+
+import (
+	"os"
+	"os/user"
+	"strconv"
+	"strings"
+
+	"github.com/tmrts/flamingo/pkg/context"
+	"github.com/tmrts/flamingo/pkg/file"
+	"github.com/tmrts/flamingo/pkg/sys"
+)
+
+const (
+	SSHDirPath         = ".ssh"
+	AuthorizedKeysPath = ".ssh/authorized_keys"
+)
+
+// Verify uses ssh-keygen utility to verify an SSH key.
+// It returns an error if a problem occurs or the key is invalid.
+// The caller should diagnose the error for more information.
+func Verify(key []byte) error {
+	tmpFile := &context.TempFile{
+		Content: string(key),
+	}
+
+	var checkSSHValidity = func(f *os.File) error {
+		_, err := sys.DefaultExecutor.Execute("ssh-keygen", "-l", "-f", f.Name())
+		return err
+	}
+
+	errch := context.Using(tmpFile, checkSSHValidity)
+
+	return <-errch
+}
+
+func InitializeFor(owner *user.User) error {
+	userSSHDirPath := owner.HomeDir + "/" + SSHDirPath
+	userAuthorizedKeysPath := owner.HomeDir + "/" + AuthorizedKeysPath
+
+	userID, err := strconv.Atoi(owner.Uid)
+	if err != nil {
+		return err
+	}
+
+	groupID, err := strconv.Atoi(owner.Gid)
+	if err != nil {
+		return err
+	}
+
+	err = file.EnsureDirectoryExists(userSSHDirPath, 0700, userID, groupID)
+	if err != nil {
+		return err
+	}
+
+	return file.EnsureExists(userAuthorizedKeysPath, 0600, userID, groupID)
+}
+
+func AuthorizeKeys(authorizedKeysFile *os.File, publicKeys ...[]byte) error {
+	var keys []string
+	for _, key := range publicKeys {
+		keys = append(keys, string(key))
+	}
+
+	_, err := authorizedKeysFile.WriteString(strings.Join(keys, "\n") + "\n")
+	return err
+}
---
+++ pkg/sys/ssh/ssh_test.go
@@ -0,0 +1,144 @@
+package ssh_test
+
+import (
+	"io/ioutil"
+	"os"
+	"os/user"
+	"path/filepath"
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+
+	"github.com/tmrts/flamingo/pkg/context"
+	"github.com/tmrts/flamingo/pkg/sys"
+	"github.com/tmrts/flamingo/pkg/sys/ssh"
+)
+
+func TestSSHKeyVerification(t *testing.T) {
+	Convey("Given an ssh key", t, func() {
+		Convey("It should validate the given key", func() {
+			Convey("For an empty SSH key", func() {
+				key := []byte("")
+
+				Convey("It should return false", func() {
+					err := ssh.Verify(key)
+					So(err, ShouldNotBeNil)
+				})
+			})
+			Convey("For a malformed SSH key", func() {
+				key := []byte("GIBBERISH")
+
+				Convey("It should return false", func() {
+					err := ssh.Verify(key)
+					So(err, ShouldNotBeNil)
+				})
+			})
+			Convey("For a valid rsa SSH public key", func() {
+				key, err := ioutil.ReadFile("test_keys/test_rsa.pub")
+				So(err, ShouldBeNil)
+
+				byteKey := []byte(key)
+
+				Convey("It should return true", func() {
+					err := ssh.Verify(byteKey)
+					So(err, ShouldBeNil)
+				})
+			})
+			Convey("For a valid rsa1 SSH public key", func() {
+				key, err := ioutil.ReadFile("test_keys/test_rsa.pub")
+				So(err, ShouldBeNil)
+
+				byteKey := []byte(key)
+
+				Convey("It should return true", func() {
+					err := ssh.Verify(byteKey)
+					So(err, ShouldBeNil)
+				})
+			})
+			Convey("For a valid dsa SSH public key", func() {
+				key, err := ioutil.ReadFile("test_keys/test_rsa.pub")
+				So(err, ShouldBeNil)
+
+				byteKey := []byte(key)
+
+				Convey("It should return true", func() {
+					err := ssh.Verify(byteKey)
+					So(err, ShouldBeNil)
+				})
+			})
+			Convey("For a valid ecdsa SSH public key", func() {
+				key, err := ioutil.ReadFile("test_keys/test_rsa.pub")
+				So(err, ShouldBeNil)
+
+				byteKey := []byte(key)
+
+				Convey("It should return true", func() {
+					err := ssh.Verify(byteKey)
+					So(err, ShouldBeNil)
+				})
+			})
+		})
+	})
+}
+
+func TestSSHDirectoryStructureInitialization(t *testing.T) {
+	Convey("Given a user", t, func() {
+		fakeHomeDir, err := ioutil.TempDir("", "flamingo-fakeuser")
+		So(err, ShouldBeNil)
+		defer os.RemoveAll(fakeHomeDir)
+
+		curUsr, err := user.Current()
+		So(err, ShouldBeNil)
+
+		fakeUser := &user.User{
+			Uid:      curUsr.Uid,
+			Gid:      curUsr.Gid,
+			Username: curUsr.Username,
+			Name:     "FakeUser",
+			HomeDir:  fakeHomeDir,
+		}
+
+		Convey("It should initialize the ssh directory for that user", func() {
+			err := ssh.InitializeFor(fakeUser)
+			So(err, ShouldBeNil)
+
+			_, err = os.Open(filepath.Join(fakeHomeDir, ".ssh"))
+			So(err, ShouldBeNil)
+
+			_, err = os.Open(filepath.Join(fakeHomeDir, ".ssh/authorized_keys"))
+			So(err, ShouldBeNil)
+		})
+	})
+}
+
+func TestSSHKeyAuthorization(t *testing.T) {
+	Convey("Given an SSH authorized keys file and public keys", t, func() {
+		rsa_key, err := ioutil.ReadFile("test_keys/test_rsa.pub")
+		So(err, ShouldBeNil)
+
+		dsa_key, err := ioutil.ReadFile("test_keys/test_dsa.pub")
+		So(err, ShouldBeNil)
+
+		tmpAuthKeysFile := &context.TempFile{
+			Content:     "",
+			Permissions: os.FileMode(0600),
+		}
+
+		Convey("It should append the SSH keys to the authorized keys file", func() {
+			authorizeKey := func(f *os.File) error {
+				err := ssh.AuthorizeKeys(f, []byte(rsa_key), []byte(dsa_key))
+				if err != nil {
+					return err
+				}
+				f.Close()
+
+				_, err = sys.DefaultExecutor.Execute("ssh-keygen", "-l", "-f", f.Name())
+
+				return err
+			}
+
+			err := <-context.Using(tmpAuthKeysFile, authorizeKey)
+			So(err, ShouldBeNil)
+		})
+	})
+}
