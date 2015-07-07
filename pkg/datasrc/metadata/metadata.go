d9f1af345cceb125293d02720aa36e03be30880d
Tamer Tas <tamertas@outlook.com>
2015-07-07 09:15:27 +0300

    Refactor MetaData Interface

---
+++ pkg/datasrc/metadata/metadata.go
@@ -0,0 +1,38 @@
+package metadata
+
+import (
+	"fmt"
+	"net"
+)
+
+type MetaData interface {
+	Digest() Digest
+}
+
+// Digest contains the summarized parts of a meta-data digest for consumption
+type Digest struct {
+	Hostname          string
+	NetworkInterfaces []Interface
+}
+
+func (d Digest) String() string {
+	return fmt.Sprintf("\nHostname: %v\nInterfaces: %v\n", d.Hostname, d.NetworkInterfaces)
+}
+
+type Interface struct {
+	NetworkName string
+
+	PrivateIP net.IP
+
+	PublicIPs []net.IP
+}
+
+func (i Interface) String() string {
+	return fmt.Sprintf("\nNetworkName: %v\nPrivateIP: %v\nPublicIPs: %v\n", i.NetworkName, i.PrivateIP, i.PublicIPs)
+}
+
+type Disk struct {
+	Name string
+	Mode string
+	Type string
+}
