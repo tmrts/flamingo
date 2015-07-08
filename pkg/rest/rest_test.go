405412c65376b526c5500ab214708870d44cce8d
Tamer Tas <tamertas@outlook.com>
2015-07-08 06:13:27 +0300

    Add RESTful client GET method tests

---
+++ pkg/rest/rest_test.go
@@ -0,0 +1,39 @@
+package rest_test
+
+import (
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+
+	"github.com/tmrts/flamingo/pkg/rest"
+)
+
+func TestRESTfulClient(t *testing.T) {
+	Convey("Given a URL and a REST client", t, func() {
+		url := "http://httpbin.org/get"
+
+		/*
+		 *client := &rest.Client{
+		 *    http.Client{
+		 *        RoundTripper: func(rq *http.Request) (*http.Response, error) {
+		 *            mockResponse := io.ReadCloser{}
+		 *            return mockResponse, nil
+		 *        },
+		 *    },
+		 *}
+		 */
+
+		client := rest.DefaultClient
+		Convey("When the client requests the contents", func() {
+			response, err := client.Get(url)
+			So(err, ShouldBeNil)
+
+			Convey("Then the response should be the raw contents of the response", func() {
+				json, err := rest.JSON(response)
+				So(err, ShouldBeNil)
+
+				So(json, ShouldEqual, "http://httpbin.org/get")
+			})
+		})
+	})
+}
