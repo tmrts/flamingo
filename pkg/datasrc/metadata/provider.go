8f9c5ddf5fc61afa3c68a28db408fcdff7b61498
Tamer Tas <tamertas@outlook.com>
2015-07-15 16:29:51 +0300

    Implement metadata provider

--- pkg/datasrc/metadata/provider.go
+++ pkg/datasrc/metadata/provider.go
@@ -1,56 +1,15 @@
 package metadata
 
-import (
-	"fmt"
+import "fmt"
 
-	"github.com/tmrts/flamingo/pkg/request"
-)
+type Version string
 
-type URL string
+type URLFormat string
 
-func (u URL) WithVersion(v Version) string {
+func (u URLFormat) WithVersion(v Version) string {
 	return fmt.Sprintf(string(u), v)
 }
 
-type ProviderType string
-
-const (
-	GCE = "GoogleComputeEngine"
-	EC2 = "ElasticComputeCloud"
-)
-
-type Source interface {
+type Provider interface {
 	MetaData(Version) (Interface, error)
-}
-
-type Provider struct {
-	Name              string
-	SupportedVersions map[Version]bool
-
-	URL URL
-}
-
-func (p *Provider) MetaData(v Version) (Interface, error) {
-	if _, ok := p.SupportedVersions[v]; ok != true {
-		return nil, fmt.Errorf("metadata: version %v for %v is not supported", v, p.Name)
-	}
-
-	url := p.URL.WithVersion(v)
-	metadata, err := request.Get(url, request.Header("Metadata-Flavor", "Google"))
-	if err != nil {
-		return nil, err
-	}
-
-	/*
-	 *switch Provider {
-	 *case "GoogleComputeEngine":
-	 */
-	var gceMetaData *GCEv1
-
-	err = metadata.JSON(gceMetaData)
-	if err != nil {
-		return nil, err
-	}
-
-	return gceMetaData, nil
 }
