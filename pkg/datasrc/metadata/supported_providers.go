b908d652d7aabc8b9a286c70e26fac0efe6c9964
Tamer Tas <tamertas@outlook.com>
2015-07-15 17:23:01 +0300

    Include supported meta-data providers

--- pkg/datasrc/metadata/supported_providers.go
+++ pkg/datasrc/metadata/supported_providers.go
@@ -1,16 +1,62 @@
 package metadata
 
-var SupportedProviders = map[ProviderType]Source{
-	GCE: &Provider{
-		Name:              "Google Compute Engine",
-		SupportedVersions: map[Version]bool{"v1": true},
+import (
+	"time"
 
-		URL: "http://metadata.google.internal/computeMetadata/%s/instance/?recursive=true",
-	},
-	EC2: &Provider{
-		Name:              "Amazon Elastic Compute Cloud",
-		SupportedVersions: map[Version]bool{"latest": true},
+	"github.com/tmrts/flamingo/pkg/datasrc/metadata/gce"
+	"github.com/tmrts/flamingo/pkg/request"
+)
 
-		URL: "http://169.254.169.254/%s/meta-data/",
-	},
+type ProviderType string
+
+// List of currently supported providers
+var SupportedProviders = map[string]Provider{
+	"GCE":       gce.MetadataService,
+	"Openstack": configdrive.Mount,
+	/*
+	 *        Name: "Google Compute Engine",
+	 *
+	 *        URL: "http://metadata.google.internal/computeMetadata/%s/instance/?recursive=true",
+	 */
+	/*
+	 *    "EC2": &Provider{
+	 *        Name: "Amazon Elastic Compute Cloud",
+	 *        SupportedVersions: []Version{
+	 *            "latest",
+	 *        },
+	 *
+	 *        URL: "http://169.254.169.254/%s/meta-data/",
+	 *    },
+	 */
+}
+
+// Get queries metadata services and returns the available metadata digest
+// Since there will be only one metadata service operational (or multiple ones returning the same),
+// it queries each known service and returns at timeout or a successful response.
+func Get(timeout time.Duration) (digest Digest) {
+	if configdrive.Available() {
+		return configdrive.Metadata()
+	}
+
+	metadataChan := make(chan Digest, len(SupportedProviders))
+	defer close(metadataChan)
+
+	go func(mdchan chan Digest) {
+		for _, p := range SupportedProviders {
+			go func(c chan Digest) {
+				metadata, err := p.Metadata(request.DefaultClient)
+				if err != nil {
+					return
+				}
+
+				c <- Digest()
+			}(mdchan)
+		}
+
+		time.Sleep(timeout)
+		c <- nil
+		close(c)
+	}(metadataChan)
+
+	return <-metadataChan
 }
