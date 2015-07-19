24d14c709fda179ce74df80625c248f8ac4d79c4
Tamer Tas <tamertas@outlook.com>
2015-07-19 09:13:54 +0300

    Implement os/user functions using nss pkg

--- pkg/sys/nss/service.go
+++ pkg/sys/nss/service.go
@@ -4,10 +4,16 @@ import (
 	"fmt"
 	"os/user"
 	"strconv"
+	"strings"
 
 	"github.com/tmrts/flamingo/pkg/sys"
 )
 
+// DefaultService is the NSS gateway that interacts with the system
+// using the default command execution process in sys.DefaultExecutor.
+var DefaultService = New(sys.DefaultExecutor)
+
+// Database represents NSS entry databases such as passwd, group, hosts, etc.
 type Database string
 
 const (
@@ -15,12 +21,17 @@ const (
 	UserShadowDatabase  Database = "shadow"
 	GroupDatabase       Database = "group"
 	GroupShadowDatabase Database = "gshadow"
+
+	HostsDatabase Database = "hosts"
 )
 
+// Service is a wrapper that can query the system NSS databases.
 type Service interface {
 	GetEntryFrom(Database, string) (string, error)
 }
 
+// Server is an NSS Service implementation
+// It takes a sys.Executor to execute nss query commands.
 type Server struct {
 	Exec sys.Executor
 }
@@ -44,14 +55,17 @@ func (s *Server) GetEntryFrom(db Database, key string) (string, error) {
 	return out, nil
 }
 
+// New returns an NSS Service using the given sys.Executor
 func New(e sys.Executor) *Server {
 	return &Server{
 		Exec: e,
 	}
 }
 
+// GetIDsForUser searches the system for the given user name and returns
+// the user ID and the primary group ID of that user.
 func GetIDsForUser(username string) (userID int, groupID int, err error) {
-	usr, err := user.Lookup(username)
+	usr, err := GetUser(username)
 	if err != nil {
 		return
 	}
@@ -67,4 +81,33 @@ func GetIDsForUser(username string) (userID int, groupID int, err error) {
 	}
 
 	return
+}
+
+// CurrentUser is an implementation of user.Current() since it's not
+// implemented in some Linux distributions.
+func GetCurrentUser() (*user.User, error) {
+	usrname, err := sys.DefaultExecutor.Execute("whoami")
+	if err != nil {
+		return nil, err
+	}
+
+	usrname = strings.TrimRight(usrname, "\n")
+
+	return GetUser(usrname)
+}
+
+// GetUser is an implementation of user.Lookup() since it's not
+// implemented in some Linux distributions.
+func GetUser(name string) (*user.User, error) {
+	ent, err := GetPasswdEntry(DefaultService, name)
+	if err != nil {
+		return nil, err
+	}
+
+	return &user.User{
+		Uid:      strconv.Itoa(ent.UID),
+		Gid:      strconv.Itoa(ent.GID),
+		Username: ent.UserName,
+		HomeDir:  ent.HomeDir,
+	}, nil
 }
