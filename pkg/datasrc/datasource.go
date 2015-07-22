f155b1a0e6d685de33b3a12d91f51e81bca1a0a5
Tamer Tas <tamertas@outlook.com>
2015-07-22 08:12:17 +0300

    Wrap meta-data and user-data retrieval under Provider interface

--- pkg/datasrc/datasource.go
+++ pkg/datasrc/datasource.go
@@ -1,1 +1,108 @@
 package datasrc
+
+import (
+	"errors"
+	"log"
+	"time"
+
+	"github.com/tmrts/flamingo/pkg/datasrc/metadata"
+	"github.com/tmrts/flamingo/pkg/datasrc/provider/gce"
+	"github.com/tmrts/flamingo/pkg/datasrc/userdata"
+)
+
+type Provider interface {
+	metadata.Provider
+	userdata.Provider
+}
+
+// SupportedProviders returns the table of supported meta-data sources.
+func SupportedProviders() map[string]Provider {
+	return map[string]Provider{
+		"GCE": &gce.MetadataService{gce.MetadataURL},
+		//"OpenStack": configdrive.Mount,
+		/*
+		 *        Name: "Google Compute Engine",
+		 *
+		 *        URL: "http://metadata.google.internal/computeMetadata/%s/instance/?recursive=true",
+		 *        "EC2": &DataSource{
+		 *            Name: "Amazon Elastic Compute Cloud",
+		 *            SupportedVersions: []Version{
+		 *                "latest",
+		 *            },
+		 *
+		 *            URL: "http://169.254.169.254/%s/meta-data/",
+		 *        },
+		 */
+	}
+}
+
+var (
+	ErrDatasourceRetrievalTimeout = errors.New("datasrc: timeout during data-source retrieval")
+)
+
+// Check tries to fetch meta-data from the given datasource provider
+// and returns the error if it encounters any.
+func Check(p Provider) error {
+	_, err := p.FetchMetadata()
+	return err
+}
+
+// FindProvider checks the given datasource providers and returns the available one.
+func FindProvider(providers map[string]Provider, timeout time.Duration) (Provider, error) {
+	providerChan := make(chan Provider)
+
+	for _, p := range providers {
+		go func() {
+			if err := Check(p); err != nil {
+				log.Print(err)
+				return
+			}
+
+			providerChan <- p
+		}()
+	}
+
+	timeoutChan := time.NewTimer(timeout).C
+
+	select {
+	case p := <-providerChan:
+		return p, nil
+	case <-timeoutChan:
+		return nil, ErrDatasourceRetrievalTimeout
+	}
+}
+
+// FetchData queries metadata services and returns the available metadata digest
+// Since there will be only one metadata service operational (or multiple ones returning the same),
+// it queries each known service and returns at timeout or a successful response.
+func FetchMetadata(datasrcs map[string]Provider, timeout time.Duration) (*metadata.Digest, error) {
+	metadataChan := make(chan *metadata.Digest)
+	errch := make(chan error)
+
+	go func() {
+		for _, ds := range datasrcs {
+			go func() {
+				_, err := ds.FetchMetadata()
+				if err != nil {
+					log.Fatalf(err.Error())
+					return
+				}
+
+				//d := *m.Digest()
+
+				//metadataChan <- d
+			}()
+		}
+
+		<-time.NewTimer(timeout).C
+
+		errch <- ErrDatasourceRetrievalTimeout
+	}()
+
+	select {
+	case md := <-metadataChan:
+		return md, nil
+	case err := <-errch:
+		return nil, err
+	}
+}
