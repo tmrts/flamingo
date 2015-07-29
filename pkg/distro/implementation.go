45e5e537b0a58af27b59dbfb3ac9d6103500d335
Tamer Tas <tamertas@outlook.com>
2015-07-29 07:08:11 +0300

    Implement centOS

---
+++ pkg/distro/implementation.go
@@ -0,0 +1,51 @@
+package distro
+
+import (
+	"github.com/tmrts/flamingo/pkg/sys"
+	"github.com/tmrts/flamingo/pkg/sys/firewall/iptables"
+	"github.com/tmrts/flamingo/pkg/sys/identity"
+	"github.com/tmrts/flamingo/pkg/sys/initd"
+	"github.com/tmrts/flamingo/pkg/sys/initd/systemd"
+	"github.com/tmrts/flamingo/pkg/sys/nss"
+)
+
+type Implementation struct {
+	sys.Executor
+
+	ID    identity.Manager
+	NSS   nss.Service
+	Initd initd.Manager
+
+	// TODO(tmrts): Change to firewall.Manager interface
+	Firewall *iptables.Implementation
+}
+
+// SetHostname changes the hostname of the given distribution.
+func (imp *Implementation) SetHostname(hostname string) error {
+	_, err := imp.Execute("hostnamectl", "set-hostname", hostname)
+
+	return err
+}
+
+// SetTimeZone changes the hostname of the given distribution.
+func (imp *Implementation) SetTimeZone(timezone string) error {
+	_, err := imp.Execute("timedatectl", "set-timezone", timezone)
+
+	return err
+}
+
+func CentOS(exec sys.Executor) *Implementation {
+	return &Implementation{
+		Executor: exec,
+
+		ID:       &identity.ManagerImplementation{exec},
+		NSS:      &nss.Server{exec},
+		Firewall: &iptables.Implementation{exec},
+		//FileSystem: &file.System{exec},
+		//Sysconf: conf.Manager hostname, timedate, etc. sysctl for CentOS
+		Initd: &systemd.Implementation{
+			UnitDir: "/etc/systemd/userexec",
+			Exec:    exec,
+		},
+	}
+}
---
+++ pkg/distro/implementation_test.go
@@ -0,0 +1,20 @@
+package distro_test
+
+import (
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestCentOSImplementation(t *testing.T) {
+	Convey("Given the distribution implementation of CentOS", t, func() {
+		/*
+		 *        mockExec := sys.NewMockExecutor()
+		 *
+		 *        centOS := distro.CentOS(mockExec)
+		 *
+		 */
+		Convey("It should do consume the given meta-data", func() {
+		})
+	})
+}
