79478801a900010a99571d776121c17de82b1c76
Tamer Tas <contact@tmrts.com>
2015-08-08 14:38:09 +0300

    Document sys.Executor

--- pkg/sys/executor.go
+++ pkg/sys/executor.go
@@ -7,23 +7,19 @@ import (
 	"os/exec"
 )
 
-var DefaultExecutor = &Linux{}
-
-func Execute(cmd string, args ...string) (string, error) {
-	return DefaultExecutor.Execute(cmd, args...)
-}
-
+// Executor is an interface representing the ability to execute given
+// a command and its arguments, and returning the output or the error.
 type Executor interface {
 	Execute(string, ...string) (string, error)
 }
 
-type Linux struct{}
+type linux struct{}
 
 // Execute wraps the command execution pattern required in os/exec package.
 // The command is executed with the supplied arguments and the output is returned
 // to the caller. If an error occurs during the command execution, an error is
 // returned with the messages read from the stderr of the executed command.
-func (l *Linux) Execute(executable string, args ...string) (string, error) {
+func (l *linux) Execute(executable string, args ...string) (string, error) {
 	cmd := exec.Command(executable, args...)
 
 	stdout, err := cmd.StdoutPipe()
@@ -62,4 +58,14 @@ func (l *Linux) Execute(executable string, args ...string) (string, error) {
 	}
 
 	return out, nil
+}
+
+// DefaultExecutor is the default Executor and is used by Execute.
+var DefaultExecutor = &linux{}
+
+// Execute executes the given command along with its arguments and the output
+// is returned to the caller. If an error occurs during the command execution,
+// an error is returned with the messages read from the stderr of the executed command.
+func Execute(cmd string, args ...string) (string, error) {
+	return DefaultExecutor.Execute(cmd, args...)
 }
