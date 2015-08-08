aa03ebf864ecec67634af833444653aada0d6c86
Tamer Tas <contact@tmrts.com>
2015-08-08 12:56:19 +0300

    Log contextualization events with flog

--- pkg/distro/context_consumer.go
+++ pkg/distro/context_consumer.go
@@ -3,7 +3,6 @@ package distro
 import (
 	"io"
 	"io/ioutil"
-	"log"
 	"os"
 	"strconv"
 	"strings"
@@ -13,6 +12,7 @@ import (
 	"github.com/tmrts/flamingo/pkg/datasrc/userdata"
 	"github.com/tmrts/flamingo/pkg/datasrc/userdata/cloudconfig"
 	"github.com/tmrts/flamingo/pkg/file"
+	"github.com/tmrts/flamingo/pkg/flog"
 	"github.com/tmrts/flamingo/pkg/sys/identity"
 	"github.com/tmrts/flamingo/pkg/sys/nss"
 	"github.com/tmrts/flamingo/pkg/sys/ssh"
@@ -33,9 +33,7 @@ func (imp *Implementation) ConsumeScript(c string) error {
 
 	return <-context.Using(tempFile, func(f *os.File) error {
 		f.Close()
-		out, err := imp.Execute("sh", f.Name())
-
-		log.Printf("ConsumeScript script output -> %v", out)
+		_, err := imp.Execute("sh", f.Name())
 
 		return err
 	})
@@ -48,16 +46,35 @@ func sTorc(s string) (rc io.ReadCloser) {
 func (imp *Implementation) consumeCloudConfig(contents string) error {
 	conf, err := cloudconfig.Parse(sTorc(contents))
 	if err != nil {
-		log.Printf("consumeCloudConfig -> consuming %v,  %v", err)
+		flog.Error("Failed to parse cloud config file",
+			flog.Fields{
+				Event: "cloudconfig.Parse",
+				Error: err,
+			},
+		)
+
 		return err
 	}
 
-	for _, f := range conf.Files {
-		log.Printf("consumeCloudConfig -> writing files %v", f.Path)
+	flog.Debug("Persisting files",
+		flog.Fields{
+			Event: "distro.consumeCloudConfig",
+		},
+	)
 
+	for _, f := range conf.Files {
 		p, err := strconv.Atoi(f.Permissions)
 		if err != nil {
-			log.Printf("consumeCloudConfig -> error writing files %v", f.Path, err)
+			flog.Error("Failed to convert permissions",
+				flog.Fields{
+					Event: "strconv.Atoi",
+					Error: err,
+				},
+				flog.Details{
+					"file":        f.Path,
+					"permissions": f.Permissions,
+				},
+			)
 			continue
 		}
 
@@ -65,64 +82,163 @@ func (imp *Implementation) consumeCloudConfig(contents string) error {
 
 		err = file.New(f.Path, file.Permissions(perms), file.Contents(f.Content))
 		if err != nil {
-			log.Printf("consumeCloudConfig -> error writing files %v", f.Path, err)
+			flog.Error("Failed to create file",
+				flog.Fields{
+					Event: "file.New",
+					Error: err,
+				},
+				flog.Details{
+					"file": f.Path,
+				},
+			)
 		}
 	}
 
 	for _, cmd := range conf.Commands {
-		log.Printf("consumeCloudConfig -> executing %v", cmd)
+		flog.Debug("Executing command",
+			flog.Fields{
+				Event: "distro.consumeCloudConfig",
+			},
+			flog.Details{
+				"command": cmd,
+			},
+		)
 
 		out, err := imp.Execute(cmd[0], cmd[1:]...)
 		if err != nil {
-			log.Printf("consumeCloudConfig -> error executing %v, %v", cmd, err)
+			flog.Error("Failed to execute command",
+				flog.Fields{
+					Event: "Implementation.Execute",
+					Error: err,
+				},
+				flog.Details{
+					"command": cmd,
+				},
+			)
 		}
 
-		log.Printf("consumeCloudConfig command output -> %v", out)
-
+		flog.Debug("Executed command",
+			flog.Fields{
+				Event: "identityManager.CreateGroup",
+			},
+			flog.Details{
+				"command": cmd,
+				"output":  out,
+			},
+		)
 	}
 
 	for grpName, _ := range conf.Groups {
-		log.Printf("consumeCloudConfig -> creating group %v", grpName)
+		flog.Info("Creating user group",
+			flog.Fields{
+				Event: "distro.consumeCloudConfig",
+			},
+			flog.Details{
+				"group": grpName,
+			},
+		)
 
 		newGrp := identity.Group{
 			Name: grpName,
 		}
 
 		if err := imp.ID.CreateGroup(newGrp); err != nil {
-			log.Printf("consumeCloudConfig -> error creating group %v, %v", grpName, err)
+			flog.Error("Failed to create a user group",
+				flog.Fields{
+					Event: "identityManager.CreateGroup",
+					Error: err,
+				},
+				flog.Details{
+					"group": grpName,
+				},
+			)
 		}
 	}
 
 	for _, usr := range conf.Users {
-		log.Printf("consumeCloudConfig -> creating group %v", usr.Name)
+		flog.Info("Creating user",
+			flog.Fields{
+				Event: "distro.consumeCloudConfig",
+			},
+			flog.Details{
+				"user": usr.Name,
+			},
+		)
 
 		if err := imp.ID.CreateUser(usr); err != nil {
-			log.Printf("consumeCloudConfig -> error creating user %v, %v", usr.Name, err)
+			flog.Error("Failed to create a user",
+				flog.Fields{
+					Event: "identityManager.CreateUser",
+					Error: err,
+				},
+				flog.Details{
+					"user": usr.Name,
+				},
+			)
 		}
 	}
 
 	for grpName, usrNames := range conf.Groups {
 		for _, usrName := range usrNames {
-			log.Printf("consumeCloudConfig -> adding user %v to group %v", usrName, grpName)
+			flog.Info("Adding user to group",
+				flog.Fields{
+					Event: "distro.consumeCloudConfig",
+				},
+				flog.Details{
+					"user":  usrName,
+					"group": grpName,
+				},
+			)
 
 			if err := imp.ID.AddUserToGroup(usrName, grpName); err != nil {
-				log.Printf("consumeCloudConfig -> error adding user %v to group %v, %v", usrName, grpName, err)
+				flog.Error("Failed to add user to group",
+					flog.Fields{
+						Event: "identityManager.AddUserToGroup",
+						Error: err,
+					},
+					flog.Details{
+						"user":  usrName,
+						"group": grpName,
+					},
+				)
 			}
 		}
 	}
 
 	for userName, sshKeys := range conf.AuthorizedKeys {
-		log.Printf("consumeCloudConfig -> authorizing ssh keys for user %v", userName)
+		flog.Info("Authorizing SSH keys",
+			flog.Fields{
+				Event: "distro.consumeCloudConfig",
+			},
+			flog.Details{
+				"user": userName,
+			},
+		)
 
 		usr, err := nss.GetUser(userName)
 		if err != nil {
-			log.Printf("consumeCloudConfig -> error retrieving entry for user %v, %v", userName, err)
+			flog.Error("Failed to retrieve user NSS entry",
+				flog.Fields{
+					Event: "nss.GetUser",
+					Error: err,
+				},
+				flog.Details{
+					"user": userName,
+				},
+			)
 			continue
 		}
 
-		log.Printf("consumeCloudConfig -> authorizing ssh keys for user %v", userName)
 		if err := ssh.AuthorizeKeysFor(usr, sshKeys); err != nil {
-			log.Printf("consumeCloudConfig -> error authorizing ssh keys for user %v, %v", userName, err)
+			flog.Error("Failed to authorize SSH keys for user",
+				flog.Fields{
+					Event: "ssh.AuthorizeKeysFor",
+					Error: err,
+				},
+				flog.Details{
+					"user": userName,
+				},
+			)
 		}
 	}
 
@@ -133,61 +249,150 @@ func (imp *Implementation) consumeCloudConfig(contents string) error {
 func (imp *Implementation) ConsumeUserdata(u userdata.Map) error {
 	// TODO(tmrts): Store unused user-data in files?
 	// TODO(tmrts): Execute scripts in rc.local or a similar level
-	log.Print("ConsumeUserdata -> entering")
+	flog.Info("Consuming user-data",
+		flog.Fields{
+			Event: "distro.ConsumeUserdata",
+		},
+	)
 
 	// TODO(tmrts): Use only scripts with 'startup', 'shutdown', 'user-data'.
 	scripts := u.Scripts()
 
 	confs := u.CloudConfigs()
 	if len(confs) > 1 {
-		log.Print("ConsumeUserdata -> more than one cloud-config file found")
+		flog.Info("Found multiple cloud-config files",
+			flog.Fields{
+				Event: "distro.ConsumeUserdata",
+			},
+		)
 	}
 
 	for name, content := range confs {
-		log.Printf("ConsumeUserdata -> consuming %v", name)
+		flog.Info("Consuming user-data file",
+			flog.Fields{
+				Event: "distro.ConsumeUserdata",
+			},
+			flog.Details{
+				"name": name,
+			},
+		)
 
 		err := imp.consumeCloudConfig(content)
 		if err != nil {
+			flog.Error("Failed to consume cloud-config file",
+				flog.Fields{
+					Event: "distro.consumeCloudConfig",
+				},
+				flog.Details{
+					"name": name,
+				},
+				flog.DebugFields{
+					"content": content,
+				},
+			)
 			return err
 		}
 	}
 
 	for name, content := range scripts {
-		log.Printf("ConsumeUserdata -> executing script %v", name)
+		flog.Info("Executing script",
+			flog.Fields{
+				Event: "distro.ConsumeUserdata",
+			},
+			flog.Details{
+				"name": name,
+			},
+		)
 
 		if err := imp.ConsumeScript(content); err != nil {
-			log.Printf("ConsumeUserdata -> error executing script %v, %v", name, err)
+			flog.Error("Failed to execute script",
+				flog.Fields{
+					Event: "distro.ConsumeScript",
+				},
+				flog.Details{
+					"name": name,
+				},
+				flog.DebugFields{
+					"content": content,
+				},
+			)
 		}
 	}
 
-	log.Print("ConsumeUserdata -> exiting")
+	flog.Info("Finished consuming user-data",
+		flog.Fields{
+			Event: "distro.ConsumeUserdata",
+		},
+	)
 
 	return nil
 }
 
 // ConsumeUserdata uses the given userdata to contextualize the distribution implementation.
 func (imp *Implementation) ConsumeMetadata(m *metadata.Digest) error {
-	log.Print("ConsumeMetadata -> entering")
+	flog.Info("Consuming meta-data",
+		flog.Fields{
+			Event: "distro.ConsumeMetadata",
+		},
+	)
 
 	if err := imp.SetHostname(m.Hostname); err != nil {
+		flog.Error("Failed to set hostname",
+			flog.Fields{
+				Event: "distro.SetHostname",
+			},
+			flog.Details{
+				"name": m.Hostname,
+			},
+		)
 		return err
 	}
 
 	for userName, sshKeys := range m.SSHKeys {
-		log.Printf("ConsumeMetadata -> authorizing ssh keys for user %v", userName)
+		flog.Info("Authorizing SSH keys",
+			flog.Fields{
+				Event: "distro.consumeMetadata",
+			},
+			flog.Details{
+				"user": userName,
+			},
+		)
 
 		usr, err := nss.GetUser(userName)
 		if err != nil {
-			log.Printf("ConsumeMetadata -> error retrieving user from nss, %v", err)
+			flog.Error("Failed to retrieve user NSS entry",
+				flog.Fields{
+					Event: "nss.GetUser",
+					Error: err,
+				},
+				flog.Details{
+					"user": userName,
+				},
+			)
 			continue
 		}
 
 		if err := ssh.AuthorizeKeysFor(usr, sshKeys); err != nil {
-			log.Printf("consumeCloudConfig -> error authorizing ssh keys for user %v, %v", userName, err)
+			flog.Error("Failed to authorize SSH keys for user",
+				flog.Fields{
+					Event: "ssh.AuthorizeKeysFor",
+					Error: err,
+				},
+				flog.Details{
+					"user": userName,
+				},
+				flog.DebugFields{
+					"SSHKeys": sshKeys,
+				},
+			)
 		}
 	}
 
-	log.Print("ConsumeMetadata -> exiting")
+	flog.Info("Finished consuming meta-data",
+		flog.Fields{
+			Event: "distro.ConsumeUserdata",
+		},
+	)
 
 	return nil
 }
